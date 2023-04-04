0f2f3a7581362c068a58653664cbb4041f82fe29
yinfenghuang <persistence@126.com>
2023-04-04 15:15:56 +0800

    add

---
+++ src/test/java/com/chengjian/webapi/WebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengjian.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
